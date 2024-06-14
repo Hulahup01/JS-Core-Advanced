9abfa82f9e77a44f39059e1641eb1774cc8ebd8b
Hulahup01 <ilya.deepak@yandex.ru>
2024-06-14 15:15:50 +0300

    add use strict

---
+++ [1] use strict/factorial.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function factorial(number) {
+    if (number <= 1)
+        return 1;
+
+    return number * factorial(number - 1);
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(10));
